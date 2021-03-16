5f47ac84742eb01dba51d34f7cd3398ec7491860
erdanli <erdanli@xiaoe-tech.com>
2021-03-16 17:42:23 +0800

    cobra is DONE

---
+++ go-daily-lib/cobra/go.mod
@@ -0,0 +1,5 @@
+module github.com/gopherli/cobra
+
+go 1.15
+
+require github.com/spf13/cobra v1.1.3
---
+++ go-daily-lib/cobra/main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/gopherli/cobra/cmd"
+)
+
+func main() {
+	cmd.Execute()
+}
